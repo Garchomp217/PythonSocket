b4ea6f6461cbe71e9237a63ccfda7be603b9cb32
Andris Mednis <andris@mednis.lv>
2019-06-09 12:24:32 +0300

    initial commit

---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(),1234))
+s.listen(5)
+
+while True:
+    cliensocket, addres = s.accept()
+    print(f"Connection from {addres} has been established!")
+    clientsocket.sen(bytes("welcome to the server!", "utf-8"))
