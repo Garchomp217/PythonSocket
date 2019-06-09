4b2e793c2b37dd582bf72e16caddcfe3934f0961
Andris Mednis <andris@mednis.lv>
2019-06-09 12:55:58 +0300

    bug fixes for server

--- server.py
+++ server.py
@@ -5,6 +5,6 @@ s.bind((socket.gethostname(), 4567))
 s.listen(5)
 
 while True:
-    cliensocket, addres = s.accept()
+    clientsocket, addres = s.accept()
     print(f"Connection from {addres} has been established!")
-    clientsocket.sen(bytes("welcome to the server!", "utf-8"))
+    clientsocket.send(bytes("welcome to the server!", "utf-8"))
